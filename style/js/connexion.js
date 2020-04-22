81bdac1eaa071c4dba5febca15e3149f18dbbde0
Laurie <lauriecoumes@gmail.com>
2020-04-22 22:42:57 +0200

    Ajout du stockage local pour toutes les info du user

--- style/js/connexion.js
+++ style/js/connexion.js
@@ -2,14 +2,14 @@ new Vue({
     el: "#input",
     data: function () {
         return {
-            username: '',
+            mail: '',
             password: '',
             display_mdp: false,
             display_mail: false
         }
     },
     template: `<div>
-                <input type="email" id="username" name="username" v-model="username" placeholder="Nom d'utilisateur" required>
+                <input type="email" id="mail" name="mail" v-model="mail" placeholder="Adresse e-mail" required>
                 <input type="password" id="password" name="password" v-model="password" placeholder="Mot de passe" required>
 
                 <button type="submit" id="submit" class="btn btn-success btn-sm" v-on:click="login">Se connecter</button>
@@ -21,6 +21,7 @@ new Vue({
                     <span aria-hidden="true" >&times;</span>
                   </button>
                 </div>
+                
                 <!-- Alert unknown user -->
                 <div class="alert alert-warning alert-dismissible fade show" id="alert" role="alert" v-if="display_mail">
                   Votre <strong>adresse mail</strong> ne correspond à aucun compte existant. Veuillez réessayer ou cliquez <a href="/inscription" class="alert-link">ici</a> pour vous inscrire.
@@ -35,31 +36,47 @@ new Vue({
     methods: {
         close_mdp: function () {
             this.display_mdp = !this.display_mdp;
-        }
-        ,
+        },
         close_mail: function () {
             this.display_mail = !this.display_mail;
-        }
-        ,
+        },
         login: function () {
             /* if the username and the password are not empty */
-            if (this.username.length > 0 && this.password.length > 0) {
+            if (this.mail.length > 0 && this.password.length > 0) {
                 axios.get('http://localhost:3000/VerifUtilisateur', {
                     params: {
-                        mail: this.username,
+                        mail: this.mail,
                     }
                 }).then(response => {
-                    console.log(response);
                     if (response.data.length !== 0) {
                         if (this.password === response.data[0].motdepass) {
-                            /* the username and the password are localy stored to create an user session  */
-                            localStorage.username = this.username;
+                            /* the mail and the password are localy stored to create an user session  */
+                            localStorage.mail = this.mail;
                             localStorage.password = this.password;
                             /* redirection */
-                            window.location.href = "/feed";
+                            window.location.href = "feed";
                             /* deletion of the alerts */
                             this.display_mdp = false;
                             this.display_mail = false;
+
+                            /* all the user information are locally stored */
+                            axios.get('http://localhost:3000/unUtilisateur', {
+                                params: {
+                                    mail: this.mail,
+                                }
+                            }).then(response => {
+                                console.log(response);
+                                if (response.data.length !== 0) {
+                                    localStorage.cp = response.data[0].CP;
+                                    localStorage.adresse = response.data[0].adresse;
+                                    localStorage.datenaiss = response.data[0].date_naissance;
+                                    localStorage.nom = response.data[0].nom;
+                                    localStorage.prenom = response.data[0].prenom;
+                                    localStorage.pays = response.data[0].pays;
+                                    localStorage.username = response.data[0].pseudo;
+                                    localStorage.ville = response.data[0].ville;
+                                }
+                            });
                         } else {
                             this.display_mdp = true;
                             this.display_mail = false;
