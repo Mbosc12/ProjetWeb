15cec887b9599cd7c75d5910820f3b499b7ad16f
Laurie <lauriecoumes@gmail.com>
2020-04-24 15:47:12 +0200

    Création vu pour graphique + ajout script pour likes (provisoire) et config (ok)

--- style/js/repart_sexe.js
+++ style/js/repart_sexe.js
@@ -2,7 +2,7 @@ new Vue({
     el: "#disconnect",
     data: function () {
         return {
-            username: localStorage.username
+            username: localStorage.username,
         }
     },
     template: `<a class="nav-link" href="connexion" v-on:click="disconnect"><i class="fas fa-power-off fa-lg"></i></a>`,
@@ -18,21 +18,93 @@ new Vue({
             localStorage.pays = "";
             localStorage.username = "";
             localStorage.ville = "";
+        },
+    }
+});
+
+new Vue ({
+    data: function() {
+        return {
+            nb_femme:"",
+            nb_homme: "",
+            nb_autre: ""
+        }
+    },
+    methods: {
+        get_nb_gender: function () {
+            axios.get('http://localhost:3000/NbFemmeHomme', {
+                params: {
+                    mail: localStorage.mail
+                }
+            }).then(response => {
+                if (response.data.length !== 0) {
+                    this.nb_femme = response.data[0].sexe;
+                    this.nb_homme = response.data[1].sexe;
+                    this.nb_autre = response.data[2].sexe;
+
+                    new Chart(document.getElementById("pieChart").getContext('2d'), {
+                        type: 'pie',
+                        data: {
+                            labels: ["Homme", "Femme", "Autre"],
+                            datasets: [{
+                                data: [this.nb_homme, this.nb_femme, this.nb_autre],
+                                backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C"],
+                                hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870"],
+                            }],
+                        },
+                        options: {
+                            responsive: true,
+                        },
+
+                    });
+                } else {
+                    console.log("erreur /NbFemmeHomme");
+                }
+            });
+
+        },
+    },
+    created: function () {
+        this.get_nb_gender();
+    }
+});
+
+
+new Vue ({
+    el: "#likes",
+    data: function() {
+        return {
+            display_notif: false
+        }
+    },
+    template: `<div><a class="dropdown-item" href="#">Prochainement</a>
+                    <a class="dropdown-item" href="#">Les notifications</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Mais pas tout de suite</a>
+                    <a class="dropdown-item" href="#">Patience</a></div>`,
+    methods: {
+        notif: function () {
+            console.log("notification");
+            this.display_notif = !this.display_notif;
         }
     }
 });
 
-new Chart(document.getElementById("pieChart").getContext('2d'), {
-    type: 'pie',
-    data: {
-        labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
-        datasets: [{
-            data: [300, 50, 100, 40, 120],
-            backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
-            hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
-        }]
+new Vue ({
+    el: "#config",
+    data: function() {
+        return {
+            display_notif: false
+        }
     },
-    options: {
-        responsive: true
+    template: `<div>
+                   <a class="dropdown-item" href="modifProfile">Modifier mes informations</a>
+                   <a class="dropdown-item" href="modifPswd">Modifier mon mot de passe</a>
+               </div>`,
+    methods: {
+        notif: function () {
+            console.log("notification");
+            this.display_notif = !this.display_notif;
+        }
     }
 });
