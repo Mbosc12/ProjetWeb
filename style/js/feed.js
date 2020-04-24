6f16e0f44612c02eb6d401ce51744138498bbc0c
Laurie <lauriecoumes@gmail.com>
2020-04-24 15:42:04 +0200

    Modification boutons likes et config + ajout dans js des likes (provisoire) et config (ok)

--- style/js/feed.js
+++ style/js/feed.js
@@ -1,3 +1,4 @@
+/* Last name and first name of the user */
 new Vue({
     el: "#connected",
     data: function () {
@@ -8,6 +9,8 @@ new Vue({
     },
     template: `<h4>Vous êtes connecté(e) en tant que : {{ nom }} {{ prenom }}</h4>`
 });
+
+/* Disconnect button and function */
 new Vue({
     el: "#disconnect",
     data: function () {
@@ -28,6 +31,47 @@ new Vue({
             localStorage.pays = "";
             localStorage.username = "";
             localStorage.ville = "";
+        }
+    }
+});
+
+/* Likes notification */
+new Vue ({
+    el: "#likes",
+    data: function() {
+        return {
+            display_notif: false
+        }
+    },
+    template: `<div><a class="dropdown-item" href="#">Prochainement</a>
+                    <a class="dropdown-item" href="#">Les notifications</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Mais pas tout de suite</a>
+                    <a class="dropdown-item" href="#">Patience</a></div>`,
+    methods: {
+        notif: function () {
+            console.log("notification");
+            this.display_notif = !this.display_notif;
+        }
+    }
+});
+
+/* Modification of the user information and password */
+new Vue ({
+    el: "#config",
+    data: function() {
+        return {
+            display_notif: false
+        }
+    },
+    template: `<div>
+                   <a class="dropdown-item" href="modifProfile">Modifier mes informations</a>
+                   <a class="dropdown-item" href="modifPswd">Modifier mon mot de passe</a>
+               </div>`,
+    methods: {
+        notif: function () {
+            console.log("notification");
+            this.display_notif = !this.display_notif;
         }
     }
 });
