b1f3b4b9c400e3b3f12e3c7bf57658654836f87d
Laurie <lauriecoumes@gmail.com>
2020-04-28 16:50:34 +0200

    Création vue pour afficher les posts

--- style/js/feed.js
+++ style/js/feed.js
@@ -7,7 +7,80 @@ new Vue({
             prenom: localStorage.prenom
         }
     },
-    template: `<h4>Vous êtes connecté(e) en tant que : {{ nom }} {{ prenom }}</h4>`
+    template: `<div id="user_card">Vous êtes connecté(e) en tant que : {{ nom }} {{ prenom }}</div>`
+});
+
+/* All the post from accounts we follow */
+const v = new Vue({
+    el: "#feed",
+    data: function () {
+        return {
+            id_post: [],
+            items: [],
+            date: []
+        }
+    },
+    template: `<div id="posts">
+                   <ul>
+                      <li v-for="item in items">
+                        <div id="user">@{{ item.user }}</div>
+                        <div id="title">{{ item.title }}</div>
+                        <div id="msg">{{ item.msg }}</div>
+                        <div id="date">{{ item.date }}</div>
+                      </li>
+                   </ul>
+               </div>`,
+    methods: {
+        get_posts: function () {
+            axios.get('http://localhost:3000/showFeed', {
+                params: {
+                    mail: localStorage.mail
+                }
+            }).then(showFeed => {
+                if (showFeed.data.length !== 0) {
+                    for (let i = 0; i < showFeed.data.length; i++) {
+                        this.id_post.push(showFeed.data[i].id_post);
+                    }
+                    for (let i = 0; i < showFeed.data.length; i++) {
+                        this.date.push(showFeed.data[i].date_publication);
+                    }
+                    for (let i = 0; i < this.id_post.length; i++) {
+                        axios.get('http://localhost:3000/unPost', {
+                            params: {
+                                postId: showFeed.data[i].id_post
+                            }
+                        }).then(unPost => {
+                            if (unPost.data.length !== 0) {
+                                /* --- User username --- */
+                                axios.get('http://localhost:3000/unUtilisateur', {
+                                    params: {
+                                        mail: unPost.data[0].FK_utilisateur_mail
+                                    }
+                                }).then(unUtilisateur => {
+                                    let d = new Date(showFeed.data[0].date_publication);
+                                    let day = d.getDate();
+                                    let month = d.getMonth() + 1;
+                                    let year = d.getFullYear();
+                                    let new_date = day + "-" + month + "-" + year;
+
+                                    /* --- Posts properties --- */
+                                    v.items.push({
+                                        user: unUtilisateur.data[0].pseudo,
+                                        title: unPost.data[0].titre,
+                                        msg: unPost.data[0].message,
+                                        date: new_date
+                                    });
+                                });
+                            }
+                        });
+                    }
+                }
+            });
+        },
+    },
+    created: function () {
+        this.get_posts();
+    }
 });
 
 /* Disconnect button and function */
@@ -26,7 +99,7 @@ new Vue({
             localStorage.cp = "";
             localStorage.adresse = "";
             localStorage.datenaiss = "",
-            localStorage.sexe = "";
+                localStorage.sexe = "";
             localStorage.nom = "";
             localStorage.prenom = "";
             localStorage.pays = "";
@@ -37,9 +110,9 @@ new Vue({
 });
 
 /* Likes notification */
-new Vue ({
+new Vue({
     el: "#likes",
-    data: function() {
+    data: function () {
         return {
             display_notif: false
         }
@@ -58,9 +131,9 @@ new Vue ({
 });
 
 /* Modification of the user information and password */
-new Vue ({
+new Vue({
     el: "#config",
-    data: function() {
+    data: function () {
         return {
             display_notif: false
         }
