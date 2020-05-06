091cdb074d2c68c76aa9e644888c9ee520865c4c
Laurie <lauriecoumes@gmail.com>
2020-05-06 15:52:30 +0200

    Ajout script allPages.js

--- style/js/feed.js
+++ style/js/feed.js
@@ -10,7 +10,7 @@ new Vue({
     template: `<div id="user_card">Vous êtes connecté(e) en tant que : {{ nom }} {{ prenom }}</div>`
 });
 
-/* All the post from accounts we follow */
+/* All the post from accounts the user follow */
 const v = new Vue({
     el: "#feed",
     data: function () {
@@ -131,72 +131,5 @@ const v = new Vue({
     },
     created: function () {
         this.get_posts();
-    }
-});
-
-/* Disconnect button and function */
-new Vue({
-    el: "#disconnect",
-    data: function () {
-        return {
-            username: localStorage.username
-        }
-    },
-    template: `<a class="nav-link" href="connexion" v-on:click="disconnect"><i class="fas fa-power-off fa-lg"></i></a>`,
-    methods: {
-        disconnect: function () {
-            localStorage.mail = "";
-            localStorage.password = "";
-            localStorage.cp = "";
-            localStorage.adresse = "";
-            localStorage.datenaiss = "",
-                localStorage.sexe = "";
-            localStorage.nom = "";
-            localStorage.prenom = "";
-            localStorage.pays = "";
-            localStorage.username = "";
-            localStorage.ville = "";
-        }
-    }
-});
-
-/* Likes notification */
-new Vue({
-    el: "#likes",
-    data: function () {
-        return {
-            display_notif: false
-        }
-    },
-    template: `<div><a class="dropdown-item" href="#">Prochainement</a>
-                    <a class="dropdown-item" href="#">Les notifications</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Mais pas tout de suite</a>
-                    <a class="dropdown-item" href="#">Patience</a></div>`,
-    methods: {
-        notif: function () {
-            console.log("notification");
-            this.display_notif = !this.display_notif;
-        }
-    }
-});
-
-/* Modification of the user information and password */
-new Vue({
-    el: "#config",
-    data: function () {
-        return {
-            display_notif: false
-        }
-    },
-    template: `<div>
-                   <a class="dropdown-item" href="modifProfile">Modifier mes informations</a>
-                   <a class="dropdown-item" href="modifPswd">Modifier mon mot de passe</a>
-               </div>`,
-    methods: {
-        notif: function () {
-            console.log("notification");
-            this.display_notif = !this.display_notif;
-        }
     }
 });
