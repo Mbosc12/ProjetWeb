95960009495962f8d57147ac46e3ab8a48d58c2a
Laurie <lauriecoumes@gmail.com>
2020-05-09 18:37:46 +0200

    Modif fonction pour notifications : classées par ordre décroissant OK

--- style/js/allPages.js
+++ style/js/allPages.js
@@ -33,148 +33,166 @@ new Vue({
             likes: [],
             comments: [],
             follow: [],
-            share: []
+            share: [],
+            notifications: []
         }
     },
     template: `
         <div id="likes">
-            <div v-for="item in likes">
-                <a class="dropdown-item">{{item.user + item.text }} <u>"{{ item.titre }}"</u>. <span
-                        id="date">{{ item.date}}</span></a>
-                <!-- <ul class="dropdown-item" v-for="item in likes">
-                    <li class="dropdown-item">
-                        {{ item.user }} a like votre post {{ item.postId }}. {{ item.titre }} le {{ item.date }}
-                    </li>
-                </ul> -->
-            </div>
-            <div v-for="item in comments">
-                <a class="dropdown-item" href="#">{{ item.user + item.text }} <u>"{{ item.titre }}"</u>. <span
-                        id="date">{{ item.date }}</span></a>
-            </div>
-            <div v-for="item in follow">
-                <a class="dropdown-item" href="#">{{ item.user + item.text }}. <span id="date">{{ item.date }}</span></a>
-            </div>
-            <div v-for="item in share">
-                <a class="dropdown-item" href="#">{{ item.user + item.text }} <u>"{{ item.titre }}"</u>. <span id="date">{{ item.date }}</span></a>
+            <div id="text" v-for="item in notifications">
+                <a class="dropdown-item"><strong>{{ item.user }}</strong>{{ item.text }}<u>{{ item.titre }}</u>.
+                    <p id="date">{{ item.date}} jours</p></a>
             </div>
         </div>`,
     methods: {
-        getLikes: function () {
-            axios.get('http://localhost:3000/notifLike', {
+        compare: function (a, b) {
+            const dateA = a.date;
+            const dateB = b.date;
+
+            let comparison = 0;
+            if (dateA > dateB) {
+                comparison = 1;
+            } else if (dateA < dateB) {
+                comparison = -1;
+            }
+            return comparison;
+        },
+        getNotifications: async function () {
+            /* -------------------- Get likes -------------------- */
+            await axios.get('http://localhost:3000/notifLike', {
                 params: {
                     mail: localStorage.mail
                 }
-            }).then(notifLike => {
+            }).then(async notifLike => {
                 for (let i = 0; i < notifLike.data.length; i++) {
-                    axios.get('http://localhost:3000/unUtilisateur', {
-                        params: {
-                            mail: notifLike.data[i].FK_utilisateur_mail
-                        }
-                    }).then(unUtilisateur => {
-                        let d = new Date(notifLike.data[i].date_like);
+                    let d = new Date(notifLike.data[i].date_like);
 
-                        const diffTime = Math.abs(this.today - d);
-                        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+                    const diffTime = Math.abs(this.today - d);
+                    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
-                        this.likes.push({
-                            user: unUtilisateur.data[0].pseudo,
-                            text: " a aimé votre post",
-                            titre: notifLike.data[i].titre,
-                            postId: notifLike.data[i].FK_post_id,
-                            date: diffDays + " jours"
-                        });
+                    this.likes.push({
+                        mail: notifLike.data[i].FK_utilisateur_mail,
+                        text: " a aimé votre post ",
+                        titre: '"' + notifLike.data[i].titre + '"',
+                        postId: notifLike.data[i].FK_post_id,
+                        date: diffDays
                     });
                 }
-            });
-        },
-        getComments: function () {
-            axios.get('http://localhost:3000/notifCom', {
-                params: {
-                    mail: localStorage.mail
-                }
-            }).then(notifCom => {
-                for (let i = 0; i < notifCom.data.length; i++) {
+                for (let j = 0; j < this.likes.length; j++) {
                     axios.get('http://localhost:3000/unUtilisateur', {
                         params: {
-                            mail: notifCom.data[i].FK_utilisateur_mail
+                            mail: this.likes[j].mail
                         }
                     }).then(unUtilisateur => {
+                        this.likes[j].user = unUtilisateur.data[0].pseudo
+                    });
+                    this.notifications.push(this.likes[j]);
+
+                }
+
+                /* -------------------- Get comments -------------------- */
+                await axios.get('http://localhost:3000/notifCom', {
+                    params: {
+                        mail: localStorage.mail
+                    }
+                }).then(async notifCom => {
+                    for (let i = 0; i < notifCom.data.length; i++) {
                         let d = new Date(notifCom.data[i].date_commentaire);
 
                         const diffTime = Math.abs(this.today - d);
                         const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
                         this.comments.push({
-                            user: unUtilisateur.data[0].pseudo,
-                            text: " a commenté votre post",
-                            titre: notifCom.data[i].titre,
+                            mail: notifCom.data[i].FK_utilisateur_mail,
+                            text: " a commenté votre post ",
+                            titre: '"' + notifCom.data[i].titre + '"',
                             postId: notifCom.data[i].FK_post_id,
-                            date: diffDays + " jours"
+                            date: diffDays
                         });
-                    });
-                }
-            });
-        },
-        getFollow: function () {
-            axios.get('http://localhost:3000/notifFollow', {
-                params: {
-                    mail: localStorage.mail
-                }
-            }).then(notifFollow => {
-                for (let i = 0; i < notifFollow.data.length; i++) {
-                    axios.get('http://localhost:3000/unUtilisateur', {
+                    }
+                    for (let j = 0; j < this.comments.length; j++) {
+                        axios.get('http://localhost:3000/unUtilisateur', {
+                            params: {
+                                mail: this.comments[j].mail
+                            }
+                        }).then(unUtilisateur => {
+                            this.comments[j].user = unUtilisateur.data[0].pseudo
+                        });
+                        this.notifications.push(this.comments[j]);
+                    }
+
+                    /* -------------------- Get follow -------------------- */
+                    await axios.get('http://localhost:3000/notifFollow', {
                         params: {
-                            mail: notifFollow.data[i].FK_utilisateur_mail_2
+                            mail: localStorage.mail
                         }
-                    }).then(unUtilisateur => {
-                        let d = new Date(notifFollow.data[i].date_follow);
+                    }).then(async notifFollow => {
+                        for (let i = 0; i < notifFollow.data.length; i++) {
+                            let d = new Date(notifFollow.data[i].date_follow);
 
-                        const diffTime = Math.abs(this.today - d);
-                        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+                            const diffTime = Math.abs(this.today - d);
+                            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
-                        this.follow.push({
-                            user: unUtilisateur.data[0].pseudo,
-                            text: " a commencé à vous suivre",
-                            date: diffDays + " jours"
+                            this.follow.push({
+                                mail: notifFollow.data[i].FK_utilisateur_mail_2,
+                                text: " a commencé à vous suivre",
+                                titre: null,
+                                date: diffDays
+                            });
+                        }
+                        for (let j = 0; j < this.follow.length; j++) {
+                            axios.get('http://localhost:3000/unUtilisateur', {
+                                params: {
+                                    mail: this.follow[j].mail
+                                }
+                            }).then(unUtilisateur => {
+                                this.follow[j].user = unUtilisateur.data[0].pseudo
+                            });
+                            this.notifications.push(this.follow[j]);
+                        }
+
+                        /* -------------------- Get share -------------------- */
+                        await axios.get('http://localhost:3000/notifPartage', {
+                            params: {
+                                mail: localStorage.mail
+                            }
+                        }).then(async notifPartage => {
+                            for (let i = 0; i < notifPartage.data.length; i++) {
+                                let d = new Date(notifPartage.data[i].date_partage);
+
+                                const diffTime = Math.abs(this.today - d);
+                                const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+                                this.share.push({
+                                    mail: notifPartage.data[i].FK_utilisateur_mail,
+                                    text: " a partagé votre post ",
+                                    titre: '"' + notifPartage.data[i].titre + '"',
+                                    postId: notifPartage.data[i].FK_post_id,
+                                    date: diffDays
+                                });
+                            }
+                            for (let j = 0; j < this.share.length; j++) {
+                                axios.get('http://localhost:3000/unUtilisateur', {
+                                    params: {
+                                        mail: this.share[j].mail
+                                    }
+                                }).then(unUtilisateur => {
+                                    this.share[j].user = unUtilisateur.data[0].pseudo;
+                                });
+                                this.notifications.push(this.share[j]);
+                            }
+
+                            /* -------------------- Get notifications -------------------- */
+                            this.notifications.sort(this.compare);
+                            return this.notifications;
                         });
                     });
-                }
-            });
-        },
-        getShare: function () {
-            axios.get('http://localhost:3000/notifPartage', {
-                params: {
-                    mail: localStorage.mail
-                }
-            }).then(notifPartage => {
-                for (let i = 0; i < notifPartage.data.length; i++) {
-                    axios.get('http://localhost:3000/unUtilisateur', {
-                        params: {
-                            mail: notifPartage.data[i].FK_utilisateur_mail
-                        }
-                    }).then(unUtilisateur => {
-                        let d = new Date(notifPartage.data[i].date_partage);
-
-                        const diffTime = Math.abs(this.today - d);
-                        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-
-                        this.share.push({
-                            user: unUtilisateur.data[0].pseudo,
-                            text: " a partagé votre post",
-                            titre: notifPartage.data[i].titre,
-                            postId: notifPartage.data[i].FK_post_id,
-                            date: diffDays + " jours"
-                        });
-                    });
-                }
+                });
             });
         }
     },
     created: function () {
-        this.getLikes();
-        this.getComments();
-        this.getFollow();
-        this.getShare();
+        this.getNotifications();
     }
 })
 ;
